58f824242261902cbdc994287186e66ddb75f296
s0141 <hanxu13@mails.tsinghua.edu.cn>
2016-04-23 20:23:39 +0800

    init

--- ucore.sh
+++ ucore.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-#gcc -o xc -O3 -m32 -Ilinux ucore/tool/c.c
-#gcc -o xem -O3 -m32 -Ilinux ucore/tool/em.c -lm
-cpp-5 -Iucore/lib ucore/kern/main.c ucore.c
+gcc -o xc -O3 -m32 -Ilinux -Iucore/lib ucore/tool/c.c
+gcc -o xem -O3 -m32 -Ilinux -Iucore/lib ucore/tool/em.c -lm
+cpp-4.9 -Iucore/lib ucore/kern/main.c ucore.c
 ./xc -v -o ucore.bin ucore.c 
-#./xem ucore.bin
+./xem ucore.bin
