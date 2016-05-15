a6140666caa0b3796ddeeebd7665972623c680ff
Leonard Xu <leopard.lie@gmail.com>
2016-05-15 10:58:49 +0800

    rm recursive os testing.

--- boot.sh
+++ boot.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
-rm -f xc xem xmkfs root/bin/c root/etc/os root/etc/sfs.img fs.img
+rm -f xc xem xmkfs root/bin/c root/etc/os fs.img
 gcc -o xc -O3 -m32 -Ilinux -Iroot/lib root/bin/c.c
 gcc -o xem -O3 -m32 -Ilinux -Iroot/lib root/bin/em.c -lm
-gcc -o xmkfs -O3 -m32 -Ilinux -Iroot/lib root/etc/mkfs.c
+gcc -o xmkfs -O3 -m32 -Ilinux -Iroot/lib root/bin/mkfs.c
 ./xc -o root/bin/c -Iroot/lib root/bin/c.c
 ./xc -o root/etc/os -Iroot/lib root/etc/os.c
-./xmkfs sfs.img root
-mv sfs.img root/etc/.
 ./xmkfs fs.img root
 ./xem -f fs.img root/etc/os
--- cleanup.sh
+++ cleanup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 rm -f xc xem xeu xmkfs fsd term
 rm -f xhello hello emhello euhello hello.txt emhello.txt euhello.txt c em eu
-rm -f fs.img root/bin/c root/etc/os root/etc/sfs.img
+rm -f fs.img root/bin/c root/etc/os
