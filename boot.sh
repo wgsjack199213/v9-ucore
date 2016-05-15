80cd484f9490ce5ee4a8509c0e85d59a8d36e999
Leonard Xu <leopard.lie@gmail.com>
2016-05-15 11:14:32 +0800

    use gcc preprocessor.

--- boot.sh
+++ boot.sh
@@ -3,7 +3,8 @@ bash cleanup.sh
 gcc -o xc -O3 -m32 -Ilinux -Iroot/lib root/bin/c.c
 gcc -o xem -O3 -m32 -Ilinux -Iroot/lib root/bin/em.c -lm
 gcc -o xmkfs -O3 -m32 -Ilinux -Iroot/lib root/bin/mkfs.c
+cpp-5 -I root/lib root/kern/main.c root/kern/os.c
 ./xc -o root/bin/c -Iroot/lib root/bin/c.c
-./xc -o os.bin -Iroot/lib root/kern/os.c
+./xc -o os.bin root/kern/os.c
 ./xmkfs fs.img root
 ./xem -f fs.img os.bin
--- cleanup.sh
+++ cleanup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 rm -f xc xem xeu xmkfs fsd term
 rm -f xhello hello emhello euhello hello.txt emhello.txt euhello.txt c em eu
-rm -f fs.img root/bin/c os.bin
+rm -f fs.img root/bin/c root/kern/os.c os.bin
