8e5a2b4e2000bdf7f9c192dcd250f394d65bfd03
Leonard Xu <leopard.lie@gmail.com>
2016-05-15 10:48:19 +0800

    remove graphics support from xv6

--- cleanup.sh
+++ cleanup.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-rm -f xc xem xeu xmkfs gld fsd term
+rm -f xc xem xeu xmkfs fsd term
 rm -f xhello hello emhello euhello hello.txt emhello.txt euhello.txt c em eu
-rm -f os0 os1 os2 os3
 rm -f fs.img root/bin/c root/etc/os root/etc/sfs.img
