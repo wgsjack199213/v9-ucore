7221c13e05c7cb7a1c11ada264c8cc7268011d6f
Leonard Xu <leopard.lie@gmail.com>
2016-05-26 23:38:01 +0800

    clean.

--- ucore.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-bash ucore-clean.sh
-gcc -o xc -O3 -m32 -Ilinux -Iucore/lib ucore/tool/c.c
-gcc -o xem -O3 -m32 -Ilinux -Iucore/lib ucore/tool/em.c -lm
-cpp-4.9 -Iucore/lib -Iucore/kern/include -Iucore/kern/libs -Iucore/kern/mm -Iucore/kern/fs -Iucore/kern/driver -Iucore/kern/sync -Iucore/kern/trap -Iucore/kern/process -Iucore/kern/schedule -Iucore/kern/syscall ucore/kern/main.c ucore.c
-./xc -v -o ucore.bin ucore.c 
-./xem ucore.bin
