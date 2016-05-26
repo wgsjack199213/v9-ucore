1c2fc49e452e56891efa8af7c0eef43f0e816ea5
HanXu <cst_hanxu13@163.com>
2016-05-26 21:28:17 +0800

    completion of lab1-6

---
+++ experiment/lab4-lab5/ucore.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+bash ucore-clean.sh
+gcc -o xc -O3 -m32 -Ilinux -Iucore/lib ucore/tool/c.c
+gcc -o xem -O3 -m32 -Ilinux -Iucore/lib ucore/tool/em.c -lm
+g++ -o changeHeader -O3 -m32 ucore/tool/changeHeader.c
+cpp-4.8 -Iucore/lib -Iucore/kern/include -Iucore/kern/libs -Iucore/kern/mm -Iucore/kern/fs -Iucore/kern/driver -Iucore/kern/sync -Iucore/kern/trap -Iucore/kern/process -Iucore/kern/schedule -Iucore/kern/syscall ucore/kern/main.c ucore.c
+cpp-4.8  -Iucore/user/libs -Iucore/lib -Iucore/kern/include ucore/user/exit.c ucore/user/bin/exit.c
+./xc -v -o ucore/user/bin/exit ucore/user/bin/exit.c
+./changeHeader ucore/user/bin/exit
+./xc -v -o ucore.bin ucore.c 
+./xem -f ucore/user/bin/exit ucore.bin
+bash ucore-clean.sh
