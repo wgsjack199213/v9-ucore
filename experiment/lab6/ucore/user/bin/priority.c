66dc105ec8b282b73724ef74b2a15aefd66f845e
Leonard Xu <leopard.lie@gmail.com>
2016-05-27 14:45:49 +0800

    use map instead of copy. define DISKBASE=0xD000000. char *memdisk

--- experiment/lab6/ucore/user/bin/priority.c
+++ experiment/lab6/ucore/user/bin/priority.c
@@ -1,8 +1,6 @@
 # 1 "ucore/user/priority.c"
 # 1 "<built-in>"
 # 1 "<command-line>"
-# 1 "/usr/include/stdc-predef.h" 1 3 4
-# 1 "<command-line>" 2
 # 1 "ucore/user/priority.c"
 # 1 "ucore/user/libs/ulib.h" 1
 
