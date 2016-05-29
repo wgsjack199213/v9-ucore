925595da40a26a0e6760541fd915d042038d1619
Leonard Xu <leopard.lie@gmail.com>
2016-05-29 12:27:38 +0800

    clean lab6

--- experiment/lab6/ucore.sh
+++ experiment/lab6/ucore.sh
@@ -3,8 +3,8 @@ bash ucore-clean.sh
 gcc -o xc -O3 -m32 -Ilinux -Iucore/lib ucore/tool/c.c
 gcc -o xem -O3 -m32 -Ilinux -Iucore/lib ucore/tool/em.c -lm
 g++ -o changeHeader -O3 -m32 ucore/tool/changeHeader.c
-cpp-4.8 -Iucore/lib -Iucore/kern/include -Iucore/kern/libs -Iucore/kern/mm -Iucore/kern/fs -Iucore/kern/driver -Iucore/kern/sync -Iucore/kern/trap -Iucore/kern/process -Iucore/kern/schedule -Iucore/kern/syscall ucore/kern/main.c ucore.c
-cpp-4.8  -Iucore/user/libs -Iucore/lib -Iucore/kern/include ucore/user/priority.c ucore/user/bin/priority.c
+cpp-5 -Iucore/lib -Iucore/kern/include -Iucore/kern/libs -Iucore/kern/mm -Iucore/kern/fs -Iucore/kern/driver -Iucore/kern/sync -Iucore/kern/trap -Iucore/kern/process -Iucore/kern/schedule -Iucore/kern/syscall ucore/kern/main.c ucore.c
+cpp-5  -Iucore/user/libs -Iucore/lib -Iucore/kern/include ucore/user/priority.c ucore/user/bin/priority.c
 ./xc -v -o ucore/user/bin/priority ucore/user/bin/priority.c
 ./changeHeader ucore/user/bin/priority
 ./xc -v -o ucore.bin ucore.c 
